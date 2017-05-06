09bcffa2e45548549d7a6e8a220a4e61b7f64fb5
SkyPotato <hunsooski@naver.com>
2017-05-06 14:48:22 +0900

    DI(1)-SpringFramwork

---
+++ src/myspring/di/xml/StringPrinter.java
@@ -0,0 +1,17 @@
+package myspring.di.xml;
+
+public class StringPrinter implements Printer {
+
+	private StringBuffer buffer = new StringBuffer();
+	
+	@Override
+	public void print(String message) {
+		// TODO Auto-generated method stub
+		buffer.append(message);
+	}
+	
+	public String toString(){
+		return buffer.toString();
+	}
+
+}
