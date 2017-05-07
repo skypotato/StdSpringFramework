c0b22dc5d1b27b31ec4d614377d68e5febe5709e
SkyPotato <hunsooski@naver.com>
2017-05-07 17:19:46 +0900

    StdProperty1

--- src/myspring/di/xml/Hello.java
+++ src/myspring/di/xml/Hello.java
@@ -1,9 +1,30 @@
 package myspring.di.xml;
 
+import java.util.List;
+
 public class Hello {
 	private String name;
 	private Printer printer;
+	private List<String> names;
 	
+	public Hello() {
+		super();
+	}
+
+	public Hello(String name, Printer printer) {
+		super();
+		this.name = name;
+		this.printer = printer;
+	}
+
+	public List<String> getNames() {
+		return names;
+	}
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
--- src/myspring/di/xml/test/HelloBeanJunitSpringTest.java
+++ src/myspring/di/xml/test/HelloBeanJunitSpringTest.java
@@ -3,13 +3,13 @@ package myspring.di.xml.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
-import org.junit.Before;
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,7 +23,7 @@ public class HelloBeanJunitSpringTest {
 	@Autowired
 	ApplicationContext context;
 
-	@Test
+	@Test@Ignore
 	public void test2() {
 		// 2. Hello Bean 가져오기
 		Hello hello = (Hello) context.getBean("hello");
@@ -33,21 +33,28 @@ public class HelloBeanJunitSpringTest {
 		assertEquals(hello, hello2);
 	}
 
-	@Ignore
 	@Test
 	public void test1() {
 		// TODO Auto-generated method stub
 		// 2. Hello Bean 가져오기
-		Hello hello = (Hello) context.getBean("hello");
+		Hello hello = (Hello) context.getBean("hello2");
 		assertEquals("Hello Spring", hello.sayHello());
 		hello.print();
+		
+		assertEquals(3, hello.getNames().size());
+		List<String> list = hello.getNames();
+		for (String value : list) {
+			System.out.println(value);
+		}
 
+		/*
 		// 3. StringPrinter Bean 가져오기
 		Printer printer = context.getBean("printer", Printer.class);
 		assertEquals("Hello Spring", printer.toString());
 
 		Hello hello2 = context.getBean("hello", Hello.class);
 		System.out.println(hello == hello2);
+		*/
 
 	}
 
