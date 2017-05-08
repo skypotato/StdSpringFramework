8953e971d52308fa26db4e4b64e9abefc1470787
SkyPotato <hunsooski@naver.com>
2017-05-08 15:19:38 +0900

    annotStd

---
+++ src/myspring/di/annot/StringPrinter.java
@@ -0,0 +1,20 @@
+package myspring.di.annot;
+
+import org.springframework.stereotype.Component;
+
+@Component("stringPrinter")
+public class StringPrinter implements Printer {
+
+	private StringBuffer buffer = new StringBuffer();
+	
+	@Override
+	public void print(String message) {
+		// TODO Auto-generated method stub
+		buffer.append(message);
+	}
+	
+	public String toString(){
+		return buffer.toString();
+	}
+
+}
